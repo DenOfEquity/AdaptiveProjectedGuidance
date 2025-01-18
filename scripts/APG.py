b6dd0b99fcceda0d0889555c15f82a776963b17a
DenOfEquity <166248528+DenOfEquity@users.noreply.github.com>
2025-01-18 18:26:50 +0000

    TraSCE

--- scripts/APG.py
+++ scripts/APG.py
@@ -1,7 +1,10 @@
 import torch
 import gradio as gr
 
-from modules import scripts
+from modules import scripts, shared
+from backend.sampling.condition import Condition, compile_conditions
+from backend.sampling.sampling_function import calc_cond_uncond_batch
+from modules.prompt_parser import SdConditioning
 from modules.ui_components import InputAccordion
 
 
@@ -66,6 +69,7 @@ class APG:
 
 class APGforForge(scripts.Script):
     sorting_priority = 11.9
+    empty = None
     
     presets_builtin = [
         #   name, eta, rescale threshold, momentum
@@ -89,26 +93,35 @@ class APGforForge(scripts.Script):
     def ui(self, *args, **kwargs):
         
         with InputAccordion(False, label=self.title()) as apg_enabled:
+            apg_method = gr.Radio(label='CFG method', choices=["APG", "TraSCE", "Normal"], value="APG")
+
             apg_eta = gr.Slider(label='eta (contrast)', minimum=-1.0, maximum=1, step=0.01, value=0.0)
             apg_r   = gr.Slider(label='rescale threshold', minimum=0, maximum=20, step=0.01, value=8.0)
             apg_m   = gr.Slider(label='momentum', minimum=-1.0, maximum=1.0, step=0.01, value=-0.5)
             with gr.Row():
-                apg_icg = gr.Slider(label='image Independent Condition Guidance', minimum=0.0, maximum=1.0, step=0.01, value=0.0) #max 0.2?
+                apg_icg = gr.Slider(label='image Independent Condition Guidance', minimum=0.0, maximum=0.2, step=0.001, value=0.0)
                 apg_icg_s = gr.Slider(label='ICG start', minimum=0.0, maximum=1.0, step=0.01, value=0.4)
             with gr.Row():
-                apg_preset = gr.Dropdown(label='', choices=[x[0] for x in APGforForge.presets], value='(presets)', type='index', scale=0, allow_custom_value=True)
+                apg_preset = gr.Dropdown(label='', choices=[x[0] for x in APGforForge.presets], value='(APG presets)', type='index', scale=0, allow_custom_value=True)
 
         def setParams (preset):
             if preset < len(APGforForge.presets):
-                return  APGforForge.presets[preset][1], APGforForge.presets[preset][2], APGforForge.presets[preset][3], \
-                        '(presets)'
+                return  'APG', APGforForge.presets[preset][1], APGforForge.presets[preset][2], APGforForge.presets[preset][3], \
+                        '(APG presets)'
             else:
-                return 0.0, 8.0, -0.5, '(presets)'
+                return 'APG', 0.0, 8.0, -0.5, '(APG presets)'
 
         apg_preset.input(fn=setParams, inputs=[apg_preset],
-                         outputs=[apg_eta, apg_r, apg_m, apg_preset], show_progress=False)
+                         outputs=[apg_method, apg_eta, apg_r, apg_m, apg_preset], show_progress=False)
+
+        def show_options (method):
+            visible = True if method == "APG" else False
+            return gr.update(visible=visible), gr.update(visible=visible), gr.update(visible=visible)
+
+        apg_method.change(fn=show_options, inputs=[apg_method], outputs=[apg_eta, apg_r, apg_m], show_progress=False)
 
         apg_enabled.do_not_save_to_config = True
+        apg_method.do_not_save_to_config = True
         apg_eta.do_not_save_to_config = True
         apg_r.do_not_save_to_config = True
         apg_m.do_not_save_to_config = True
@@ -117,6 +130,7 @@ class APGforForge(scripts.Script):
 
         self.infotext_fields = [
             (apg_enabled, lambda d: d.get("APG_enabled", False)),
+            (apg_method,   "APG_method"),
             (apg_eta,      "APG_eta"),
             (apg_r,        "APG_r"),
             (apg_m,        "APG_m"),
@@ -124,25 +138,29 @@ class APGforForge(scripts.Script):
             (apg_icg_s,    "APG_ICG_start"),
         ]
 
-        return apg_enabled, apg_eta, apg_r, apg_m, apg_icg, apg_icg_s
+        return apg_enabled, apg_method, apg_eta, apg_r, apg_m, apg_icg, apg_icg_s
         
     def process(self, p, *script_args, **kwargs):
-        apg_enabled, apg_eta, apg_r, apg_m, apg_icg, apg_icg_s = script_args
+        apg_enabled, apg_method, apg_eta, apg_r, apg_m, apg_icg, apg_icg_s = script_args
 
         if apg_enabled:
             p.extra_generation_params.update(dict(
                 APG_enabled   = apg_enabled,
-                APG_eta       = apg_eta,
-                APG_r         = apg_r,
-                APG_m         = apg_m,
+                APG_method    = apg_method,
                 APG_ICG       = apg_icg,
                 APG_ICG_start = apg_icg_s,
             ))
+            if apg_method == "APG":
+                p.extra_generation_params.update(dict(
+                    APG_eta       = apg_eta,
+                    APG_r         = apg_r,
+                    APG_m         = apg_m,
+                ))
 
         return
 
     def process_before_every_sampling(self, p, *script_args, **kwargs):
-        apg_enabled, apg_eta, apg_r, apg_m, apg_icg, apg_icg_s = script_args
+        apg_enabled, apg_method, apg_eta, apg_r, apg_m, apg_icg, apg_icg_s = script_args
 
         if not apg_enabled:
             return
@@ -153,17 +171,32 @@ class APGforForge(scripts.Script):
             
             def sampler_apg(args):
                 input = args["input"]
-                cond = input - args["cond"]
-                uncond = input - args["uncond"]
+                cond = args["cond_denoised"]
+                uncond = args["uncond_denoised"]
                 cond_scale = args["cond_scale"]
                 sigma = args["sigma"]
+                options = args["model_options"]
                 
-                if icg > 0 and sigma <=start:
+                if icg > 0 and sigma <= start:
+                    factor = icg
+                    if apg_method != "APG":
+                        factor *= 0.1
+                    factor *= min(1.0, sigma)
+
                     icg_uncond = torch.randn_like(uncond)
                     icg_uncond *= uncond.std()
-                    torch.lerp(uncond, icg_uncond, icg, out=uncond)
+                    torch.lerp(uncond, icg_uncond, factor, out=uncond)
 
-                return input - apg.normalized_guidance(cond, uncond, cond_scale, apg.momentum, apg.eta, apg.r)
+                match apg_method:
+                    case "APG":
+                        denoised =  apg.normalized_guidance(cond, uncond, cond_scale, apg.momentum, apg.eta, apg.r)
+                    case "TraSCE":
+                        bias, _ = calc_cond_uncond_batch(model.model, APGforForge.empty, None, input, sigma, options)
+                        denoised = bias + cond_scale * (cond - uncond)
+                    case _:
+                        denoised = uncond + cond_scale * (cond - uncond)
+
+                return input - denoised
 
             m = model.clone()
             m.set_model_sampler_cfg_function(sampler_apg)
@@ -171,10 +204,12 @@ class APGforForge(scripts.Script):
 
 
         unet = p.sd_model.forge_objects.unet
-
         unet = patch(unet, apg_eta, apg_r, apg_m, apg_icg, apg_icg_s)[0]
-
         p.sd_model.forge_objects.unet = unet
+
+        empty_prompt = SdConditioning([""], is_negative_prompt=False, width=p.width, height=p.height)
+        empty_cond = shared.sd_model.get_learned_conditioning(empty_prompt)
+        APGforForge.empty = compile_conditions(empty_cond)
 
         return
 
